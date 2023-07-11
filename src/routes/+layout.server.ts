682d18a33f7a47dcb41b63089c7a7c67808ac9f0
Alex van der Valk <21194068+alexvdvalk@users.noreply.github.com>
2023-07-11 16:57:09 +0100

    set region to UK

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,10 @@
 import type { LayoutServerLoad } from "./$types";
+import type { Config } from "@sveltejs/adapter-vercel";
+
+export const config: Config = {
+  runtime: "nodejs18.x",
+  regions: [`lhr1`],
+};
 
 export const load = (async ({ locals }) => {
   return {
